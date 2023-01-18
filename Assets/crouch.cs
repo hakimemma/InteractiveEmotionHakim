12bacc470055de35f2ff8c72bf940a502062de1e
hakimemma <113105266+hakimemma@users.noreply.github.com>
2023-01-18 12:28:32 +0000

    starting of collectables

--- Assets/crouch.cs
+++ Assets/crouch.cs
@@ -7,51 +7,7 @@ public class crouch : MonoBehaviour
     public CharacterController PlayerHeight;
     public CapsuleCollider playerCol;
     public float normalHeight, crouchHeight;
-
-
-    bool Crouching = false;
-    
-    void Update(){
-        if(Input.GetKey(KeyCode.C) && Crouching == false)
-        {
-            PlayerHeight.height = crouchHeight;
-            Crouching = true;
-            Debug.Log("RO");
-        }
-        if(Input.GetKey(KeyCode.C) && Crouching == true)
-        {
-             PlayerHeight.height = normalHeight;
-             Debug.Log("OG");
-             Crouching = false;
-        }   
-    }
-    
-        
-    
-//
-    public Transform player;
-    public Vector3 offset;
-    public bool crouched;
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.C)){
-            crouched = !crouched;
-            if (crouched)
-            {
-                transform.localScale = new Vector3(transform.localScale.x, crouchHeight, transform.localScale.z);
-                //PlayerHeight.height = crouchHeight;
-                //playerCol.height = crouchHeight;
-                //player.position = player.position + offset;
-
-            } else
-            {
-                transform.localScale = new Vector3(transform.localScale.x, normalHeight, transform.localScale.z);
-                //PlayerHeight.height = normalHeight;
-                //playerCol.height = normalHeight;
-            }
-            
-        }
+  
 //
     public Transform player;
     public Vector3 offset;

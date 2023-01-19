cd8eab5276b2e0dc661362ab0954db5a6ca9366f
hakimemma <113105266+hakimemma@users.noreply.github.com>
2023-01-19 12:41:29 +0000

    chest and collectable p2

--- Assets/collectable.cs
+++ Assets/collectable.cs
@@ -13,6 +13,6 @@ public class collectable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      transform.localRotation = Quaternion.Euler(90f, Time.time * 100f,0);
     }
 }

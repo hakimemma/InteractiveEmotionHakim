20eea69737afa1f2a31ca31be6457fdecc9e65ad
hakimemma <113105266+hakimemma@users.noreply.github.com>
2023-02-07 12:46:29 +0000

    2nd room ted

--- Assets/scripts/collectable.cs
+++ Assets/scripts/collectable.cs
@@ -19,7 +19,7 @@ public class collectable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localRotation= Quaternion.Euler(90f,Time.time*100f,0);
+        
     }
 
     private void OnTriggerStay(Collider other)
